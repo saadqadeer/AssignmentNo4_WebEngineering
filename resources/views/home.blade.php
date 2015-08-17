31327214bb1aaf74b418573bbe36b15f4d64c4a1
saadqadeer <saadwiz@gmail.com>
2015-08-17 20:49:28 +0500

    Laravel Simple Auth and DashBoard
    Simple laravel auth and dashboarrd with clock calendar weather and task
    manager configured with pouchdb

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+@extends('app')
+
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="col-md-10 col-md-offset-1">
+			<div class="panel panel-default">
+				<div class="panel-heading">Home</div>
+
+				<div class="panel-body">
+				<a href="index.html">DashBoard</a>
+				<h1>Muhammad Saad Qadeer</h1>
+				<h1>Web Engineering</h1>
+				<h1>Computer Science</h1>
+					You are logged in!
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
+
+
+
